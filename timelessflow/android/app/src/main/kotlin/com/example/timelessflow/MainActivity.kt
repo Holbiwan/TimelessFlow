0ea6bc9b6a51ddce265e35a450b4de381f86bdb2
Holbiwan <sabrinapapeau@gmail.com>
2025-03-03 20:01:54 +0100

    Update files

---
+++ timelessflow/android/app/src/main/kotlin/com/example/timelessflow/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.timelessflow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ timelessflow/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Timeless Flow',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Bienvenue sur Timeless Flow")),
+      body: Center(child: Text("Firebase est bien connecté 🚀")),
+    );
+  }
+}
